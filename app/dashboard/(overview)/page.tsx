523aabb1988a13acf2c27b7deda1a9ba9974cac4
iveyopt <93614550+iveyopt@users.noreply.github.com>
2024-10-08 13:02:52 -0400

    Update page.tsx

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,8 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
-import { RevenueChartSkeleton,
+import { 
+       RevenueChartSkeleton,
        LatestInvoicesSkeleton,
 } from '@/app/ui/skeletons';
  
