a2192964df2f31f8ff2746d2ce495deceed4bf9a
iveyopt <93614550+iveyopt@users.noreply.github.com>
2024-10-08 11:50:53 -0400

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,11 +2,15 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchRevenue, fetchLatestInvoices } from '@/app/lib/data';
+import { fetchRevenue, fetchLatestInvoices, fetchPaidInvoices, fetchPendingInvoices, fetchNumberOfInvoices, fetchNumberOfCustomers } from '@/app/lib/data';
  
 export default async function Page() {
  const revenue = await fetchRevenue();
  const latestInvoices = await fetchLatestInvoices();
+ const paidInvoices = await fetchPaidInvoices();
+ const pendingInvoices = await fetchPendingInvoices();
+ const allInvoices = await fetchNumberOfInvoices();
+ const totalCustomers await fetchNumberOfCustomers();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
